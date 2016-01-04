2594e40f9e1abc3786fe1f3fb64516306f39c2db
viktorHbenitez <viktorHbenitez@hotmail.com>
2016-01-03 22:25:16 -0600

    Clase 14 ng-repeat 1

--- js/app.js
+++ js/app.js
@@ -1,32 +1,11 @@
-//modulo global de la interfaz
-var app = angular.module('universidadApp',[]);
-
-//manejan una parte de la pagina o su totalidad
-app.controller('profesorCtrl',function($scope){
-    $scope.profesor = profesoresData;
-    $scope.editando = {};//variable de scope vacia
-    $scope.MostrarCambios=false;//ng-hiden ng-show
+(function(){//funcion encapsulada
+    var app= angular.module('universidadApp', []);
     
-    $scope.editarProferos = function(){
-        angular.copy($scope.profesor,$scope.editando);
-        $scope.MostrarCambios=true;
-    }
-    $scope.GuardarCambio = function(){
-        angular.copy($scope.editando,$scope.profesor);
-        $scope.MostrarCambios=false;
-    }
-    $scope.CancelarCambio = function(){
-        $scope.editando={};
-        $scope.MostrarCambios=false;
-    }
+    app.controller('listadoCtrl',['$scope',function($scope){
+        
+        $scope.listado = ["viktor hugo benitez", "julio cesar benitez", "alfonso roman benitez", "maria perez"];
+        
+        
+    }]);
     
-});
-
-
-
-var profesoresData= {
-    nombre: "Juan Carlos Pineda",
-    bio: "Saludos estudiante, mi nombre es Juan Carlos, encantado de conocerte, soy una apasionado instructor de matemáticas aplicadas cuánticas, más orientado a la física termonuclear. Mi vocación es ser maestro y lograr transmitir mis conocimientos a todos mis estudiantes!.", 
-    edad:"27",
-    foto:"img/juancarlos.jpg"
-}
+})();
