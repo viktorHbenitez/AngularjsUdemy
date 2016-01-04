ee37c31df5b31d1833a8fcabc81fb80a3d429884
viktorHbenitez <viktorHbenitez@hotmail.com>
2016-01-03 23:05:32 -0600

    Clase 15 ng-repeat parte 2

--- js/app.js
+++ js/app.js
@@ -2,8 +2,26 @@
     var app= angular.module('universidadApp', []);
     
     app.controller('listadoCtrl',['$scope',function($scope){
-        
+        //arreglo de valores String
         $scope.listado = ["viktor hugo benitez", "julio cesar benitez", "alfonso roman benitez", "maria perez"];
+        
+        //Arreglo de objetos javascript
+        $scope.listadoProfesores ={
+            profesores:[{
+                nombre: "viktor hugo benitez",
+                edad:"30",
+                clase:"programacion"
+            },{
+                nombre:"julio cesar benitez",
+                edad:"27",
+                clase: "reposteria"
+            },{
+                nombre:"alfonso roman benitez bosques",
+                edad:"34",
+                clase: "ingeniero"
+            }]
+        }
+        
         
         
     }]);
