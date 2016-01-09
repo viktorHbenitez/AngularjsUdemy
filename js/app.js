8a8c3ffa145a1564e430e2d51d98b507b48be12f
Victor Benitez <victorHbenitez85@hotmail.com>
2016-01-08 23:55:07 -0600

    Terminacion de la clase 19  - request

--- js/app.js
+++ js/app.js
@@ -6,7 +6,15 @@ var app = angular.module('ejemplosApp',[ ]);
 
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
   
-  
+  	$scope.profesores = {};
+
+  	$http.get('json/profesores.json').success(function(data){
+
+  		$scope.profesores = data.profesores;
+
+  	});
+
+
 
 
   
