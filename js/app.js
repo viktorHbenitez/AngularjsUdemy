103224559bcee7eac87d4c71524503079412f3f1
Victor Benitez <victorHbenitez85@hotmail.com>
2016-01-08 23:58:39 -0600

    Clase 23 ng-include inicio de la clase

--- js/app.js
+++ js/app.js
@@ -6,18 +6,15 @@ var app = angular.module('ejemplosApp',[ ]);
 
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
   
-  	$scope.profesores = {};
+  
+	$scope.profesores = {};
 
+  
   	$http.get('json/profesores.json').success(function(data){
-
+  		//Codigo cuando es correcta la petición: peticion ajax
   		$scope.profesores = data.profesores;
 
   	});
-
-
-
-
-  
 
 
 
