c1fef9cb69066ce7035a37d652c9d2246a7dc2fb
Victor Benitez <victorHbenitez85@hotmail.com>
2016-01-09 16:39:04 -0600

    Terminacion de seccion 5

--- js/app.js
+++ js/app.js
@@ -6,14 +6,16 @@ var app = angular.module('ejemplosApp',[ ]);
 
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
   
+  //http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&callback=JSON_CALLBACK
+	$scope.clima = {};
 
-  $scope.geo = {};
-  //http://www.geoplugin.net/json.gp?jsoncallback=JSON_CALLBACK
-	$http.jsonp('http://www.geoplugin.net/json.gp?jsoncallback=JSON_CALLBACK')
-	.success(function(data){
-			$scope.geo = data;
+	$http.jsonp('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&callback=JSON_CALLBACK&appid=9f50a805aa0089a1edd1829a5db029f0')
+		.success(function(data){
 
-	});
+			$scope.clima = data;
+
+
+		});
 
 
 
