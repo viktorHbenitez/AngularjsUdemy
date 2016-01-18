5630eb100597daf48debd01ef5b3981d2a4b7142
Victor Benitez <victorHbenitez85@hotmail.com>
2016-01-18 14:37:00 -0600

    Clase 19  terminada

--- js/app.js
+++ js/app.js
@@ -8,10 +8,14 @@ app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
   
   
 
-
+		$scope.profesores ={};
   
 
-
+		$http.get('json/profesores.json').success(function(data){
+			//codigo cuando es correcta la peticion
+			$scope.profesores = data.profesores;
+				
+		});
 
 }]);
 
